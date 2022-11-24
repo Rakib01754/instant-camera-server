587b09342ed77fd3309958c08a7cd1e513b3c7d2
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-11-24 15:42:30 +0600

    mongodb basic connection setup done

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.port || 5000;
+require('dotenv').config()
 
 
 // middlewares
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n58ahyf.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Camera server running')
